d6686b22bbf2c32fbbe4a77eea6e544c2575b5c8
Ashley Burns <ash_burn999@yahoo.com>
2021-11-25 16:26:45 -0500

    just added ideas and thoughts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import {Route, Switch, Redirect} from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Quotes from "./components/Quote";
 
 function App() {
   return (
     <div className="App">
       <div> NavBar </div>
+      <div id='react-search'>Drop down bar
+        one for the version of the bible,
+        one for the book,
+        one for the chapter,
+        one for the verse(optional)</div>
       <Switch>
         <Route path='/quotes'>
-          <Quotes/>
+          <Quotes />
         </Route>
 
         <Route exact path='/'>
-          <Redirect to='/quotes'/>
+          <Redirect to='/quotes' />
         </Route>
       </Switch>
     </div>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,16 @@
 //http://quotes.rest/bible/verse.json
-import Axios from 'axios'
+
+// Will be using my own back end for this. this is only a tester to use temporary
+import Axios from 'axios';
 // action Type
-export const SET_FETCHING_QUOTE = 'SET_FETCHING_QUOTE'
-export const SET_QUOTE_SUCCESS = 'SET_QUOTE_SUCCESS'
+export const SET_FETCHING_QUOTE = 'SET_FETCHING_QUOTE';
+export const SET_QUOTE_SUCCESS = 'SET_QUOTE_SUCCESS';
 // action creator
 export const getQuote = () => (dispatch) => {
-    dispatch({type: SET_FETCHING_QUOTE})
+    dispatch({ type: SET_FETCHING_QUOTE });
     Axios.get('http://quotes.rest/bible/verse.json')
         .then(res => {
-            console.log('res', res.data)
-            dispatch({type: SET_QUOTE_SUCCESS, payload: res.data})
-        })
-}
+            console.log('res', res.data);
+            dispatch({ type: SET_QUOTE_SUCCESS, payload: res.data });
+        });
+};
